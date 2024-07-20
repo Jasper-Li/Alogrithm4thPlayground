3c2724177ab5befec05359b6a47a8c8861b79a08
Li Xinghe <lixinghe66@gmail.com>
2024-07-20 12:08:28 +0800

    evening

--- src/main/java/chapter1/section1/Exercises.java
+++ src/main/java/chapter1/section1/Exercises.java
@@ -1,5 +1,7 @@
 package chapter1.section1;
 
+import static java.lang.System.out;
+
 public class Exercises {
     public static int lg(int n) {
         if(n<1) {
@@ -13,4 +15,46 @@ public class Exercises {
         }
         return count > 0 ? --count : count;
     }
+    public static int[] historigram(int[] a, int M){
+        int[] result = new int[M];
+        for(var value : a) {
+            if (value >=0 && value <M) {
+                ++result[value];
+            }
+        }
+        return result;
+    }
+    public static int rank_22(int key, int[] a) {
+        return rank_22(key, a, 0, a.length-1, 0) ;
+    }
+    private static int rank_22(int key, int[] a, int low, int high, int depth) {
+        out.println(STR."\{" ".repeat(depth)}low: \{low}, high: \{high}");
+        if(low > high) return -1;
+        final var mid = (low + high) / 2;
+        final var nextDepth = ++depth;
+        if(key < a[mid]) {
+            return rank_22(key, a, low, mid-1, nextDepth);
+        } else if(key > a[mid]) {
+            return rank_22(key, a, mid+1, high, nextDepth);
+        } else {
+            return key;
+        }
+    }
+    public static int rank_23(int key, int[] a) {
+        return rank_23(key, a, 0, a.length-1, 0) ;
+    }
+    private static int rank_23(int key, int[] a, int low, int high, int depth) {
+//        out.println(STR."\{" ".repeat(depth)}low: \{low}, high: \{high}");
+        if(low > high) return -1;
+        final var mid = (low + high) / 2;
+        final var nextDepth = ++depth;
+        if(key < a[mid]) {
+            return rank_23(key, a, low, mid-1, nextDepth);
+        } else if(key > a[mid]) {
+            return rank_23(key, a, mid+1, high, nextDepth);
+        } else {
+            return key;
+        }
+    }
+
 }
--- src/test/java/chapter1/section1/ExercisesTest.java
+++ src/test/java/chapter1/section1/ExercisesTest.java
@@ -1,17 +1,25 @@
 package chapter1.section1;
 
+import edu.princeton.cs.algs4.In;
 import org.junit.jupiter.api.Test;
 
 import javax.annotation.processing.SupportedAnnotationTypes;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
-import static chapter1.section1.Exercises.lg;
+import static chapter1.section1.Exercises.*;
 import static java.lang.StringTemplate.STR;
 import static java.lang.System.out;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ExercisesTest {
     final static double LOG2BASE = Math.log(2);
+    final static String testDir = "src/test/testData";
+    final static String tinyT = testDir + "/tinyT.txt";
+    final static String tinyW = testDir + "/tinyW.txt";
     @Test
     void e14lg() {
         var random = new Random();
@@ -30,4 +38,76 @@ public class ExercisesTest {
     int expectedlg(int n) {
         return (int)(Math.log(n)/LOG2BASE);
     }
+    @Test
+    void e16() {
+//        out.println(STR."n=6: \{exR1(6)}";);
+        assertEquals("311361142246", exR1(6));
+    }
+    private static String exR1(int n){
+        if(n<=0) return "";
+        return exR1(n-3) + n +exR1(n-2) +n;
+    }
+    @Test
+    void e15() {
+        record Check(int[] a, int M, int[] expect){}
+        List<Check> checks = List.of(
+            new Check(new int[]{1, 2, 2, 3, 4, 4, 4}, 5, new int[]{0, 1, 2, 1, 3}),
+            new Check(new int[]{}, 5, new int[]{0, 0, 0, 0, 0}),
+            new Check(new int[]{1, 2, 2, 3, 4, 4, 4, 5, -1}, 5, new int[]{0, 1, 2, 1, 3}),
+            new Check(new int[]{0, 0, 0, 0, 0}, 1, new int[]{5}),
+            new Check(new int[]{100, 200, 300}, 5, new int[]{0, 0, 0, 0, 0}),
+            new Check(
+                new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+                5,
+                new int[]{0, 1, 1, 1, 1}
+            )
+        );
+        for(var check : checks) {
+            assertArrayEquals(
+                check.expect,
+                historigram(check.a, check.M),
+                STR."Failed to check: \{Arrays.toString(check.a)}"
+            );
+        }
+    }
+    @Test
+    void binaryBasic() {
+        assertTrue(new File(tinyT).exists());
+        assertTrue(new File(tinyW).exists());
+    }
+
+    @Test
+    void e22() {
+        final int[] whiteLists = In.readInts(tinyW);
+        Arrays.sort(whiteLists);
+        List<Integer>  whiteLists2 = new ArrayList<>();
+        for(int v : whiteLists)  whiteLists2.add(v);
+        final int[] keys = In.readInts(tinyT);
+        for(final var key : keys) {
+            if(rank_22(key, whiteLists) == -1) {
+                assertFalse(whiteLists2.contains(key));
+                out.println(key);
+            }
+        }
+    }
+    @Test
+    void e23() {
+        final int[] whiteLists = In.readInts(tinyW);
+        Arrays.sort(whiteLists);
+        List<Integer>  whiteLists2 = new ArrayList<>();
+        for(int v : whiteLists)  whiteLists2.add(v);
+        final int[] keys = In.readInts(tinyT);
+        List<Integer> inList = new ArrayList<>();
+        List<Integer> notInList = new ArrayList<>();
+        for(final var key : keys) {
+            if(rank_23(key, whiteLists) == -1) {
+                assertFalse(whiteLists2.contains(key));
+                notInList.add(key);
+            } else {
+                inList.add(key);
+            }
+        }
+        assertEquals(List.of(50, 99, 13), notInList);
+    }
+
 }
